0a9f5485dcaaf6450baaf8c40279c72ee25de36d
DALLERY Siegfried <sdallery@hdm.re>
2023-05-05 15:17:26 +0400

    MIse en place de la liaison entre l'API et le formulaire d'inscription

--- lib/Pages/Inscription.dart
+++ lib/Pages/Inscription.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class InscriptionPage extends StatelessWidget {
   const InscriptionPage({super.key});
@@ -21,6 +23,35 @@ class InscriptionContainer extends StatefulWidget {
 
 class _InscriptionContainerState extends State<InscriptionContainer> {
   final _formInscr = GlobalKey<FormState>();
+  final _email = TextEditingController();
+  final _pseudo = TextEditingController();
+  final _passwordUser = TextEditingController();
+  final _telephone = TextEditingController();
+  final _passwordVisible = false;
+  inscri() async {
+    print(_pseudo.text);
+    print(_email.text);
+    print(_passwordUser.text);
+    print(_telephone.text);
+    var headers = {'Content-Type': 'application/json'};
+    var request =
+        http.Request('POST', Uri.parse('http://127.0.0.1:8000/api/inscrip'));
+    request.body = json.encode({
+      "pseudo": _pseudo.text,
+      "email": _email.text,
+      "passwordUser": _passwordUser.text,
+      "telephone": _telephone.text
+    });
+    request.headers.addAll(headers);
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,37 +59,48 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
         child: Container(
           child: Column(
             children: [
-              Expanded(child: Container()),
+              Expanded(
+                child: Container(
+                  color: Colors.blueGrey,
+                ),
+              ),
               Form(
                 key: _formInscr,
                 child: Column(
                   children: [
-                    TextField(
+                    TextFormField(
+                      controller: _email,
                       decoration: InputDecoration(
-                        border: OutlineInputBorder(),
                         labelText: 'Email',
+                        icon: Icon(Icons.email_rounded),
                       ),
                     ),
-                    TextField(
+                    TextFormField(
+                      controller: _pseudo,
                       decoration: InputDecoration(
-                        border: OutlineInputBorder(),
                         labelText: 'Pseudo',
+                        icon: Icon(Icons.account_box_rounded),
                       ),
                     ),
-                    TextField(
+                    TextFormField(
+                      obscureText: !_passwordVisible,
+                      controller: _passwordUser,
                       decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Telephone',
-                      ),
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
                         labelText: 'Mot de passe',
+                        icon: Icon(Icons.password_rounded),
+                      ),
+                    ),
+                    TextFormField(
+                      controller: _telephone,
+                      decoration: InputDecoration(
+                        labelText: 'Téléphone',
+                        icon: Icon(Icons.phone_rounded),
                       ),
                     ),
                     ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        inscri();
+                      },
                       child: const Text('Inscrire'),
                     )
                   ],
