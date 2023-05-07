1a99b9ea5e022d943e6e0bcabf218e5d7b287be4
ZephirSD <siegfrieddallery@gmail.com>
2023-05-07 11:41:54 +0400

    Changements de redirections pour le controller userController

--- mongoServer/routes/auth.js
+++ mongoServer/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, signIn } = require("../controllers/messageControllers");
+const { signUp, signIn } = require("../controllers/userControllers");
 const {  
   isRequestValidated,
   validateSignUpRequest,
