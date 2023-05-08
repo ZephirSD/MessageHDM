9f3f480b3d2dab982fddeca6ad69bcb40de2f004
ZephirSD <siegfrieddallery@gmail.com>
2023-05-08 11:41:24 +0400

    Création du composant pour l'Evenement

---
+++ lib/Composants/CaseEvent.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CaseEvent extends StatelessWidget {
+  final String title;
+  final int index;
+  CaseEvent(this.title, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 25),
+      child: GestureDetector(
+        onTap: () {
+          print('test${index}');
+        },
+        child: Container(
+          height: 150,
+          color: Colors.deepPurple[400],
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
