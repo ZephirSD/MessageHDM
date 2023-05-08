ac76f554231971e4e61423553c78f2a8f789fda5
ZephirSD <siegfrieddallery@gmail.com>
2023-05-08 11:38:15 +0400

    Ajout du package `flutter_localizations` pour le changement de langue

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_localization
 import realm
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterLocalizationPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalizationPlugin"))
   RealmPlugin.register(with: registry.registrar(forPlugin: "RealmPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
