d13d1495d47f56f4d639f45bc218b455e628e408
ZephirSD <siegfrieddallery@gmail.com>
2023-06-03 15:43:02 +0400

    Création du controller pour les messages

---
+++ mongoServer/controllers/messagesControllers.js
@@ -0,0 +1,15 @@
+const Messages = require('../models/Messages');
+
+const getMessagesEvents = ((req, res) => {
+    Messages.find({evenements: req.body.eveneements})
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({msg: error}))
+})
+
+const postMessagesEvents = ((req, res) => {
+    Messages.create(req.body)
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({msg: error}))
+})
+
+module.exports = { getMessagesEvents, postMessagesEvents };
