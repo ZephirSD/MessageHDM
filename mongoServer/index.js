b8b124ca6e68b66f68bb0fc476797162ad4f7e66
ZephirSD <siegfrieddallery@gmail.com>
2023-06-03 15:45:05 +0400

    Insertion des routes dans l'`index.js`

--- mongoServer/index.js
+++ mongoServer/index.js
@@ -4,9 +4,11 @@ const connectDB = require("./db/connect");
 const app = express();
 var cors = require("cors");
 const authRouter = require("./routes/auth");
+const messagesRouter = require("./routes/messageRoutes");
 app.use(cors());
 app.use(express.json());
 app.use("/api", authRouter);
+app.use("/api/messages", messagesRouter);
 const port = 8000;
 const start = async () => {
   try {
