4f839cf9bd74171d988d8d24135195d0f2450de6
ZephirSD <siegfrieddallery@gmail.com>
2023-06-03 15:59:19 +0400

    Création du controller des `Evenements`

---
+++ mongoServer/controllers/evenementsControllers.js
@@ -0,0 +1,21 @@
+const Evenements = require('../models/Evenements');
+
+const getEvents = ((req, res) => {
+    Evenements.find({})
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({msg: error}))
+})
+
+const getOneEvent = ((req, res) => {
+    Evenements.find({_id: req.body.eventID})
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({msg: error}))
+})
+
+const eventsCreate = ((req, res) => {
+    Evenements.create(req.body)
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({msg: error}))
+})
+
+module.exports = { getEvents, getOneEvent, eventsCreate };
