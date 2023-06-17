60ce21431a4999a51f4f6fd1189ec6bff971f0af
ZephirSD <siegfrieddallery@gmail.com>
2023-06-17 13:44:59 +0400

    Ajout du système de StreamBuilder des messages et d'envoi de messages

--- lib/Pages/MessagePages.dart
+++ lib/Pages/MessagePages.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:messagehdm/Pages/EvenementAccueil.dart';
-// import 'package:stream_chat_flutter/stream_chat_flutter.dart';
+import 'package:messagehdm/Composants/BulleMessage.dart';
+import 'package:messagehdm/Composants/Messages/messages_class.dart';
+import 'package:session_next/session_next.dart';
+import 'package:http/http.dart' as http;
+import 'EvenementAccueil.dart';
+
+TextEditingController _messagesEnvoi = TextEditingController();
 
 class MessagePage extends StatelessWidget {
   final String titreEvent;
@@ -15,6 +22,66 @@ class MessagePage extends StatelessWidget {
   }
 }
 
+final String _rpcUrl =
+    Platform.isAndroid ? 'http://10.0.2.2:8000' : 'http://127.0.0.1:8000';
+var session = SessionNext();
+var eventString = session.get('event');
+var url = Uri.parse('${_rpcUrl}/api/messages/${eventString}');
+Future<List<Messages>> fetchMessages() async {
+  List<Messages> messages = [];
+  messages.clear();
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    var jsonList = json.decode(response.body);
+    if (jsonList["result"] is List<dynamic>) {
+      for (final listMessages in jsonList["result"]) {
+        messages.add(
+          Messages(
+            auteur: listMessages["auteur"],
+            texte: listMessages["texte"],
+            dateEnvoi: listMessages["date_envoi"],
+            status: listMessages["status"],
+            evenement: listMessages["evenement"],
+          ),
+        );
+      }
+    }
+    return messages;
+  } else {
+    throw Exception('Request Failed.');
+  }
+}
+
+envoiMessage() async {
+  var headers = {'Content-Type': 'application/json'};
+  var request =
+      http.Request('POST', Uri.parse('http://localhost:8000/api/messages/'));
+  request.body = json.encode({
+    "auteur": session.get("email"),
+    "evenement": session.get("event"),
+    "status": "lu",
+    "texte": _messagesEnvoi.text,
+    "date_envoi": DateTime.now().toString()
+  });
+  request.headers.addAll(headers);
+
+  http.StreamedResponse response = await request.send();
+
+  if (response.statusCode == 200) {
+    _messagesEnvoi.clear();
+  } else {
+    print(response.reasonPhrase);
+    _messagesEnvoi.clear();
+  }
+}
+
+Stream<List<Messages>> getMessages(Duration refreshTime) async* {
+  while (true) {
+    await Future.delayed(refreshTime);
+    yield await fetchMessages();
+  }
+}
+
 class MessageHome extends StatefulWidget {
   final String titreEvent;
   MessageHome(this.titreEvent);
@@ -24,36 +91,109 @@ class MessageHome extends StatefulWidget {
 }
 
 class _MessageHomeState extends State<MessageHome> {
+  final scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blueGrey,
-      child: Column(
-        children: [
-          ElevatedButton(
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text(widget.titreEvent),
+          leading: ElevatedButton(
             onPressed: () => {
-              Navigator.pushReplacement(
-                context,
-                PageRouteBuilder(
-                  pageBuilder: (context, animation1, animation2) =>
-                      EventPageAccueil(),
-                  transitionDuration: Duration.zero,
-                  reverseTransitionDuration: Duration.zero,
+              session.remove('event'),
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => EventPageAccueil(),
                 ),
               ),
             },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(
+                Colors.blueGrey,
+              ),
+            ),
             child: const Icon(Icons.arrow_back_ios),
           ),
-          Text(
-            widget.titreEvent,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24,
-            ),
+        ),
+        body: StreamBuilder(
+            stream: getMessages(Duration(seconds: 1)),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final messages = snapshot.data!;
+                return Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: ListView.builder(
+                    reverse: false,
+                    itemCount: messages.length,
+                    itemBuilder: (context, index) {
+                      final message = messages[index];
+                      return BulleMessage(
+                          message.texte,
+                          DateTime.parse(message.dateEnvoi),
+                          message.auteur,
+                          session.get("email"));
+                    },
+                  ),
+                );
+              } else {
+                return SizedBox(
+                  width: double.infinity,
+                  height: double.infinity,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                );
+              }
+            }),
+        bottomNavigationBar: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          child: Row(
+            children: [
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(35.0),
+                      border: Border.all(color: Colors.blueGrey, width: 1)),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 30,
+                        height: 10,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: _messagesEnvoi,
+                          decoration: InputDecoration(
+                              hintText: "Entrez le message...",
+                              hintStyle: TextStyle(color: Colors.grey[500]),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(width: 15),
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color: Colors.blueGrey, shape: BoxShape.circle),
+                child: InkWell(
+                  child: Icon(
+                    Icons.send_rounded,
+                    color: Colors.white,
+                  ),
+                  onTap: () {
+                    envoiMessage();
+                  },
+                ),
+              )
+            ],
           ),
-          // const StreamMessageInput(),
-        ],
-      ),
-    );
+        ));
   }
 }
