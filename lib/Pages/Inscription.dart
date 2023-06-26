1feb8409747777d8f1e02fdbab8695db153f05c2
ZephirSD <siegfrieddallery@gmail.com>
2023-06-26 13:26:14 +0400

    Modification du visuel de la page d'Inscription

--- lib/Pages/Inscription.dart
+++ lib/Pages/Inscription.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:messagehdm/Pages/Connexion.dart';
 import 'dart:convert';
 import '../Composants/InputForm.dart';
+import '../main.dart';
 
 class InscriptionPage extends StatelessWidget {
   const InscriptionPage({super.key});
@@ -84,41 +85,87 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          child: Column(
-            children: [
-              Expanded(
-                child: Container(
-                  color: Colors.blueGrey,
-                ),
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        shadowColor: Colors.transparent,
+        leading: ElevatedButton(
+          onPressed: () => {
+            Navigator.pushReplacement(
+              context,
+              PageRouteBuilder(
+                pageBuilder: (context, animation1, animation2) => MyApp(),
+                transitionDuration: Duration.zero,
+                reverseTransitionDuration: Duration.zero,
               ),
-              Form(
-                key: _formInscr,
-                child: Column(
-                  children: [
-                    InputForm("Email", Icon(Icons.email_rounded), _email),
-                    InputForm(
+            ),
+          },
+          child: Icon(Icons.arrow_back),
+        ),
+      ),
+      body: Container(
+        color: Colors.blueGrey,
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Form(
+              key: _formInscr,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child:
+                        InputForm("Email", Icon(Icons.email_rounded), _email),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: InputForm(
                         "Pseudo", Icon(Icons.account_box_rounded), _pseudo),
-                    InputForm(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: InputForm(
                       "Mot de passe",
                       Icon(Icons.password_rounded),
                       _passwordUser,
                       hiddenPassword: true,
                     ),
-                    InputForm(
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: InputForm(
                         "Téléphone", Icon(Icons.phone_rounded), _telephone),
-                    ElevatedButton(
-                      onPressed: () {
-                        inscri();
-                      },
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      inscri();
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(Colors.deepPurple),
+                      shadowColor: MaterialStatePropertyAll(Colors.transparent),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.0),
+                        ),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 5),
                       child: const Text('Inscrire'),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
