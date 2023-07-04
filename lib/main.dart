d2766c3052b177eb4f4723dc2d99db2e2e9d806a
ZephirSD <siegfrieddallery@gmail.com>
2023-07-04 07:10:14 +0400

    Ajout de `MyHttpOverrides` pour vérifier le certificat SSL pour effectuer le fetch de l'API.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:messagehdm/Composants/BoutonAccueil.dart';
 import 'package:messagehdm/Pages/Connexion.dart';
@@ -5,7 +6,17 @@ import 'package:messagehdm/Pages/Inscription.dart';
 import 'package:messagehdm/Pages/SwitchPage.dart';
 import 'package:session_next/session_next.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  HttpOverrides.global = new MyHttpOverrides();
   runApp(MyApp());
 }
 
