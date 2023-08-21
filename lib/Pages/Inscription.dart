544db99b4469f8dc80af8daf46582148c038e133
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 08:34:18 +0400

    Modification de la fonction pour l'inscription

--- lib/Pages/Inscription.dart
+++ lib/Pages/Inscription.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:messagehdm/Pages/Connexion.dart';
 import 'dart:convert';
 import '../Composants/InputForm.dart';
+import '../Ressources/couleurs.dart';
 import '../main.dart';
 
 class InscriptionPage extends StatelessWidget {
@@ -30,6 +31,7 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
   final _email = TextEditingController();
   final _pseudo = TextEditingController();
   final _passwordUser = TextEditingController();
+  final _passwordValid = TextEditingController();
   final _telephone = TextEditingController();
   final String _rpcUrl =
       Platform.isAndroid ? 'https://10.0.2.2:8000' : 'https://127.0.0.1:8000';
@@ -38,6 +40,7 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
     _email.clear();
     _pseudo.clear();
     _passwordUser.clear();
+    _passwordValid.clear();
     _telephone.clear();
   }
 
@@ -46,47 +49,57 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
     // print(_email.text);
     // print(_passwordUser.text);
     // print(_telephone.text);
-    var body = {
-      "pseudo": _pseudo.text,
-      "email": _email.text,
-      "passwordUser": _passwordUser.text,
-      "telephone": _telephone.text
-    };
     var url = Uri.parse('${_rpcUrl}/api/inscrip');
-    http
-        .post(url,
-            headers: {"Content-Type": "application/json"},
-            body: json.encode(body))
-        .then((response) {
-      print(response.statusCode);
-      if (response.statusCode == 200) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Vous êtez inscrit ${_pseudo.text}'),
-          ),
-        );
-        clearControllers();
-        Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => ConnexionPage()),
-        );
-      } else {
-        var message = json.decode(response.body);
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(message["message"]),
-          ),
-        );
-        clearControllers();
-      }
-    });
+    if (_passwordUser.text == _passwordValid.text) {
+      var body = {
+        "pseudo": _pseudo.text,
+        "email": _email.text,
+        "passwordUser": _passwordUser.text,
+        "telephone": _telephone.text
+      };
+      http
+          .post(url,
+              headers: {"Content-Type": "application/json"},
+              body: json.encode(body))
+          .then((response) {
+        print(response.statusCode);
+        if (response.statusCode == 200) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text('Vous êtez inscrit ${_pseudo.text}'),
+            ),
+          );
+          clearControllers();
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ConnexionPage()),
+          );
+        } else {
+          var message = json.decode(response.body);
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(message["message"]),
+            ),
+          );
+          clearControllers();
+        }
+      });
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Mot de passe incorrecte'),
+        ),
+      );
+      _passwordUser.clear();
+      _passwordValid.clear();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: CouleursPrefs.couleurPrinc,
         shadowColor: Colors.transparent,
         leading: ElevatedButton(
           onPressed: () => {
@@ -103,7 +116,7 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
         ),
       ),
       body: Container(
-        color: Colors.blueGrey,
+        color: CouleursPrefs.couleurPrinc,
         width: double.infinity,
         height: double.infinity,
         child: Column(
@@ -117,30 +130,49 @@ class _InscriptionContainerState extends State<InscriptionContainer> {
                   Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 25, vertical: 10),
-                    child:
-                        InputForm("Email", Icon(Icons.email_rounded), _email),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 25, vertical: 10),
                     child: InputForm(
-                        "Pseudo", Icon(Icons.account_box_rounded), _pseudo),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 25, vertical: 10),
-                    child: InputForm(
-                      "Mot de passe",
-                      Icon(Icons.password_rounded),
-                      _passwordUser,
-                      hiddenPassword: true,
+                      "Email",
+                      _email,
+                      iconInput: Icon(Icons.email_rounded),
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.symmetric(
                         horizontal: 25, vertical: 10),
                     child: InputForm(
-                        "Téléphone", Icon(Icons.phone_rounded), _telephone),
+                      "Pseudo",
+                      _pseudo,
+                      iconInput: Icon(Icons.account_box_rounded),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: InputForm(
+                      "Nouveau mot de passe",
+                      _passwordUser,
+                      hiddenPassword: true,
+                      iconInput: Icon(Icons.password_outlined),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: InputForm(
+                      "Confirmer le mot de passe",
+                      _passwordValid,
+                      hiddenPassword: true,
+                      iconInput: Icon(Icons.password_outlined),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 10),
+                    child: InputForm(
+                      "Téléphone",
+                      _telephone,
+                      iconInput: Icon(Icons.phone_rounded),
+                    ),
                   ),
                   ElevatedButton(
                     onPressed: () {
