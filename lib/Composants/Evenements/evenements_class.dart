8bfcc0885d819348b77af71879cd6676919321ef
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 09:22:48 +0400

    Modification de syntaxe pour les paramètres de la classe.

--- lib/Composants/Evenements/evenements_class.dart
+++ lib/Composants/Evenements/evenements_class.dart
@@ -6,22 +6,24 @@ class Evenements {
   final String modeEvent;
   final String createEvent;
 
-  Evenements(
-      {required this.idEvent,
-      required this.nomEvent,
-      required this.dateDebut,
-      required this.dateFin,
-      required this.modeEvent,
-      required this.createEvent});
+  Evenements({
+    required this.idEvent,
+    required this.nomEvent,
+    required this.dateDebut,
+    required this.dateFin,
+    required this.modeEvent,
+    required this.createEvent,
+  });
 
   factory Evenements.fromJson(Map<String, dynamic> json) {
     return Evenements(
-        idEvent: json["_id"],
-        nomEvent: json['nom_event'],
-        dateDebut: json['date_debut'],
-        dateFin: json['date_fin'],
-        modeEvent: json['mode_event'],
-        createEvent: json['create_event']);
+      idEvent: json["_id"],
+      nomEvent: json['nom_event'],
+      dateDebut: json['date_debut'],
+      dateFin: json['date_fin'],
+      modeEvent: json['mode_event'],
+      createEvent: json['create_event'],
+    );
   }
 
   static List<Evenements> fromJsonList(dynamic jsonList) {
