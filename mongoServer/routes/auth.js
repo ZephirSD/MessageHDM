918a0463f5abe06783c625a019e7394e663c7f65
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 09:40:21 +0400

    Création des routes pour les modifications de compte  en fonction de l'utilisateur

--- mongoServer/routes/auth.js
+++ mongoServer/routes/auth.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { signUp, signIn } = require("../controllers/userControllers");
+const { signUp, signIn, modifUser, getUser, getAllUsers, modifPassword } = require("../controllers/userControllers");
 const {  
   isRequestValidated,
   validateSignUpRequest,
   validateSignIpRequest,
 } = require("../validators/valide");
+const securite = require('../middleware/verifToken');
 
 
 router.route("/connec").post(validateSignIpRequest, isRequestValidated, signIn);
-
-
 router.route("/inscrip").post(validateSignUpRequest, isRequestValidated, signUp);
-
+router.get("/getAllUser", securite.checkJWT, getAllUsers);
+router.get("/getUser/:userID", securite.checkJWT, getUser);
+router.put("/modifUser/:userID", securite.checkJWT, modifUser);
+router.put("/modifPassword/:userID", securite.checkJWT, modifPassword);
 
 module.exports = router;
