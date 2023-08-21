3eec34fe7a33ffad4d566aef2808ba762df61798
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 09:30:58 +0400

    Création des routes pour les différents types de documents en fonction de l'utilisateur

--- mongoServer/routes/documentsRoutes.js
+++ mongoServer/routes/documentsRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
-const { getDocumentsRecents, getListesDocuments, postDocuments, deleteDocuments, getDocumentById } = require('../controllers/documentsControllers');
+const { getDocumentsRecents, getListesDocuments, getListesDocumentsPDF, getListesDocumentsImages, getListesDocumentsWord, getListesDocumentsExcel, postDocuments, deleteDocuments, getDocumentById, modifyDocuments } = require('../controllers/documentsControllers');
 
 const securite = require('../middleware/verifToken');
 
 router.get('/recents/:userID', securite.checkJWT, getDocumentsRecents);
 router.get('/listes/:userID', securite.checkJWT, getListesDocuments);
+router.get('/listes/pdf/:userID', securite.checkJWT, getListesDocumentsPDF);
+router.get('/listes/images/:userID', securite.checkJWT, getListesDocumentsImages);
+router.get('/listes/word/:userID', securite.checkJWT, getListesDocumentsWord);
+router.get('/listes/excel/:userID', securite.checkJWT, getListesDocumentsExcel);
 router.get('/recup/:docID', securite.checkJWT, getDocumentById);
 router.post('/', securite.checkJWT, postDocuments);
-router.delete('/:idDoc', securite.checkJWT, deleteDocuments);
+router.put('/modifDoc/:idDoc', securite.checkJWT, modifyDocuments);
+router.delete('/suppDoc/:idDoc', securite.checkJWT, deleteDocuments);
 
 module.exports = router;
