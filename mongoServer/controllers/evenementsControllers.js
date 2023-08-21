284fd7a313f4b0d87cc74af61939476062763fbc
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 10:24:59 +0400

    Création des controllers pour l'acceptation & le refus d'évènements privés

--- mongoServer/controllers/evenementsControllers.js
+++ mongoServer/controllers/evenementsControllers.js
@@ -1,4 +1,6 @@
 const Evenements = require('../models/Evenements');
+const Messages = require('../models/Messages');
+const moment = require('moment');
 
 const getEvents = ((req, res) => {
     Evenements.find({})
@@ -13,9 +15,82 @@ const getOneEvent = ((req, res) => {
 })
 
 const eventsCreate = ((req, res) => {
-    Evenements.create(req.body)
-        .then(result => res.status(200).json({ result }))
-        .catch(error => res.status(500).json({msg: error}))
+    Evenements.create(
+        {
+            nom_event: req.body.nom_event,
+            date_debut: moment(req.body.date_debut).toDate(),
+            date_fin: moment(req.body.date_fin).toDate(),
+            mode_event: req.body.mode_event,
+            create_event: req.body.create_event,
+            invite_prive: req.body.invite_prive
+        }
+    )
+    .then(result => res.status(200).json({ result }))
+    .catch(error => res.status(500).json({msg: error}))
 })
 
-module.exports = { getEvents, getOneEvent, eventsCreate };
+const deleteEvents = ((req, res) => {
+    Evenements.findByIdAndDelete({_id: req.params.eventID})
+    .then(
+        result => res.status(200).json({ result }),
+        Messages.findByIdAndDelete({evenement: req.params.eventID})
+        .then(result => res.status(200).json({ result }))
+        .catch(error => res.status(500).json({msg: error}))
+    )
+    .catch(error => res.status(500).json({msg: error}))
+});
+
+const modifAccepInvite = ((req, res) => {
+    const conditions = { _id: req.params.eventID, 'invite_prive.pseudo': req.body.pseudo}
+    Evenements.findOneAndUpdate(conditions, { $set: { 'invite_prive.$.accept': true}}, { new: true }).select('invite_prive')
+    .then(acceptValide => {
+        if (acceptValide) {
+            res.status(200).json({message: 'Vous avez validé l\'invitation.'});
+        } else {
+          res.status(500).json({msg: 'Vous n\'avez pas validé l\'invitation.'});
+        }
+      })
+      .catch(error => {
+        res.status(500).json({msg: error});
+      });
+});
+
+const refusAccepInvite = ((req, res) => {
+    const conditions = { _id: req.params.eventID, 'invite_prive.pseudo': req.body.pseudo}
+    Evenements.findOneAndUpdate(conditions, { $pull: { invite_prive: { "pseudo": req.body.pseudo }}})
+    .then(acceptRefus => {
+        if (acceptRefus) {
+            res.status(200).json({message: 'Vous avez refusé l\'invitation.'});
+        }
+      })
+      .catch(error => {
+        res.status(500).json({msg: error});
+      });
+});
+
+const eventInvitePrive = ((req, res) => {
+    Evenements.aggregate([{
+        $match: {
+            $or: [
+                { mode_event: 'public' },
+                { $and: [
+                    { mode_event: 'prive' },
+                    { $or: [
+                        { create_event: req.body.createEvent },
+                        { invite_prive: {
+                            $elemMatch: {
+                                pseudo: req.body.pseudoEvent,
+                                accept: true,
+                            }
+                        }},
+                    ]}
+                ]}
+            ]
+            
+        },
+    }])
+    .then(result => res.status(200).json({ result }))
+    .catch(error => res.status(500).json({msg: error}))
+});
+
+module.exports = { getEvents, getOneEvent, eventsCreate, deleteEvents, modifAccepInvite, eventInvitePrive, refusAccepInvite };
--- mongoServer/routes/evenementsRoutes.js
+++ mongoServer/routes/evenementsRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const { getEvents, getOneEvent, eventsCreate, deleteEvents } = require('../controllers/evenementsControllers');
+const { getEvents, getOneEvent, eventsCreate, modifAccepInvite, eventInvitePrive, refusAccepInvite } = require('../controllers/evenementsControllers');
 
 const securite = require('../middleware/verifToken');
 
 router.get('/', securite.checkJWT, getEvents);
+router.post('/event-invite', securite.checkJWT, eventInvitePrive);
 router.get('/:eventID', securite.checkJWT,getOneEvent);
 router.post('/', securite.checkJWT, eventsCreate);
-router.delete('/:eventID', securite.checkJWT, deleteEvents);
+router.put('/invite/:eventID', securite.checkJWT, modifAccepInvite);
+router.put('/invite-refus/:eventID', securite.checkJWT, refusAccepInvite);
+// router.delete('/:eventID', securite.checkJWT, deleteEvents);
 
 module.exports = router;
