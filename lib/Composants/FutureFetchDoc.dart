4af25f82073e012bec28d487cdc58892e4f508b2
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 09:54:20 +0400

    Création du composant pour l'affichage de la liste des documments

---
+++ lib/Composants/FutureFetchDoc.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'CaseDocument.dart';
+import 'Documents/documents_class.dart';
+
+class FutureFetchDoc extends StatefulWidget {
+  final Future<List<Documents>>? functFetch;
+  final String titre;
+  FutureFetchDoc(this.titre, {this.functFetch});
+
+  @override
+  State<FutureFetchDoc> createState() => _FutureFetchDocState();
+}
+
+class _FutureFetchDocState extends State<FutureFetchDoc> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              children: [
+                Align(
+                  alignment: AlignmentDirectional(-0.65, 0),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    child: Text(
+                      widget.titre,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  height: 200,
+                  child: FutureBuilder(
+                    future: widget.functFetch,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Documents>> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.data == null ||
+                            snapshot.data?.length == 0) {
+                          return Container(
+                            color: Color.fromARGB(107, 52, 52, 188),
+                            child: const Center(
+                              child: Text(
+                                'Aucun document',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 15),
+                              ),
+                            ),
+                          );
+                        }
+                        return ListView.builder(
+                            padding: const EdgeInsets.symmetric(horizontal: 25),
+                            scrollDirection: Axis.horizontal,
+                            itemCount: snapshot.data?.length ?? 0,
+                            itemBuilder: (context, index) {
+                              return CaseDocuments(
+                                  snapshot.data![index].nomFichier,
+                                  snapshot.data![index].idDoc,
+                                  snapshot.data![index].extensionFichier,
+                                  snapshot.data![index].idDoc,
+                                  snapshot.data![index].dataDocument);
+                            });
+                      }
+                      return const Center(
+                        child: CircularProgressIndicator(
+                          backgroundColor: Color.fromARGB(255, 14, 56, 129),
+                          color: Color.fromARGB(255, 82, 111, 255),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
