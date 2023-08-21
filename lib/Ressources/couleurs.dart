14dac3ca3a3e40ea1542f9cdfe5b412cdc1a4524
ZephirSD <siegfrieddallery@gmail.com>
2023-08-21 08:20:41 +0400

    Création du fichier pour les couleurs de l'application

---
+++ lib/Ressources/couleurs.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CouleursPrefs {
+  static const Color couleurPrinc = Colors.blueGrey;
+  static const Color couleurRose = Color.fromRGBO(229, 115, 115, 1);
+  static const Color bleuCaseBleu = Color.fromRGBO(107, 157, 182, 1);
+}
