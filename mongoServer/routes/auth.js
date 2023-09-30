af9409f5fcf0e625313ea519ca405e5249b25958
ZephirSD <siegfrieddallery@gmail.com>
2023-09-30 15:19:57 +0400

    Ajout de commentaires

--- mongoServer/routes/auth.js
+++ mongoServer/routes/auth.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const router = express.Router();
+// Export des fonctions de controller
 const { signUp, signIn, modifUser, getUser, getAllUsers, modifPassword } = require("../controllers/userControllers");
+// Export de vérification des champs du formulaire
 const {  
   isRequestValidated,
   validateSignUpRequest,
   validateSignIpRequest,
 } = require("../validators/valide");
+// Export de la fonction pour la vérification du token
 const securite = require('../middleware/verifToken');
-
-
+// Routes pour `utilisateurs`
 router.route("/connec").post(validateSignIpRequest, isRequestValidated, signIn);
 router.route("/inscrip").post(validateSignUpRequest, isRequestValidated, signUp);
 router.get("/getAllUser", securite.checkJWT, getAllUsers);
