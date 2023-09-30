305ae4d5c8c39af3651a87d73007713e4d100891
ZephirSD <siegfrieddallery@gmail.com>
2023-09-30 15:16:38 +0400

    Ajout de commentaires

--- mongoServer/index.js
+++ mongoServer/index.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const path = require('path');
 const https = require('https');
 var cors = require("cors");
+
+
 const authRouter = require("./routes/auth");
 const messagesRouter = require("./routes/messageRoutes");
 const eventsRouter = require("./routes/evenementsRoutes");
@@ -19,14 +21,21 @@ app.use("/api/messages", messagesRouter);
 app.use("/api/evenements", eventsRouter);
 app.use("/api/documents", documentRouter);
 app.use("/api/notifications", notificationRouter);
-const key = fs.readFileSync(path.join(__dirname, 'certificate', 'server.key'));
-const cert = fs.readFileSync(path.join(__dirname, 'certificate', 'server.cert'));
-const options = { key, cert };
 
+// Lecture du fichier server.key pour obtenir la clé du certificat
+const key = fs.readFileSync(path.join(__dirname, 'certificate', 'server.key'));
+// Lecture du fichier server.cert pour obtenir le certificat SSL
+const cert = fs.readFileSync(path.join(__dirname, 'certificate', 'server.cert'));
+// Création des options avec la clé et le certificat
+const options = { key, cert };
+// Le port du serveur NodeJS
 const port = 8000;
+// Fonction pour démarrer le serveur NodeJS
 const start = async () => {
   try {
+    // Connexion vers la base de données MessageHDMBase MongoDB 
     await connectDB(process.env.MONGO_CONNECTION).then(() => {
+      // Création du serveur avec le protocole HTTPS
       https.createServer(options, app).listen(port, () => {
         console.log(`Vous êtes sur le port ${port}`);
       });
