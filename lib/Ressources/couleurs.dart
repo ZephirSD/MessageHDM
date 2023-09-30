8585fa73bfe474407f2c492813b07ed44dc419fb
ZephirSD <siegfrieddallery@gmail.com>
2023-09-30 15:24:27 +0400

    Ajout de nouveaux variables de couleurs

--- lib/Ressources/couleurs.dart
+++ lib/Ressources/couleurs.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 class CouleursPrefs {
-  static const Color couleurPrinc = Colors.blueGrey;
-  static const Color couleurRose = Color.fromRGBO(229, 115, 115, 1);
+  static const Color couleurPrinc = Color.fromRGBO(30, 69, 146, 1);
+  static const Color couleurRose = Color.fromRGBO(231, 34, 61, 1);
   static const Color bleuCaseBleu = Color.fromRGBO(107, 157, 182, 1);
+  static const Color couleurGrisFonce = Color.fromRGBO(87, 87, 87, 1);
+  static const Color couleurGrisClair = Color.fromRGBO(192, 192, 192, 1);
+  static const Color couleurBlanc = Colors.white;
+  static const Color couleurNoir = Colors.black;
 }
