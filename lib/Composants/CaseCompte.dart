c67b71dedc9bed043c79fa2451b72444cb611fbb
ZephirSD <siegfrieddallery@gmail.com>
2023-10-04 20:52:38 +0400

    Ajout d'options pour les différents variants possibles pour le composant `CaseCompte`

--- lib/Composants/CaseCompte.dart
+++ lib/Composants/CaseCompte.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:messagehdm/Ressources/couleurs.dart';
 
-class CaseCompte extends StatelessWidget {
+// ignore: must_be_immutable
+class CaseCompte extends StatefulWidget {
   final String texte;
   final Widget? redirection;
   final Function funSupp;
-  CaseCompte(this.texte, {this.redirection = null, this.funSupp = _test});
+  final bool boolPage;
+  final bool boolPopUp;
+  final bool toggle;
+  bool activeToggle;
+  CaseCompte(this.texte,
+      {this.redirection = null,
+      this.boolPage = true,
+      this.boolPopUp = false,
+      this.toggle = false,
+      this.activeToggle = true,
+      this.funSupp = _test});
 
+  @override
+  State<CaseCompte> createState() => _CaseCompteState();
+}
+
+class _CaseCompteState extends State<CaseCompte> {
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -35,55 +51,57 @@ class CaseCompte extends StatelessWidget {
                 hoverColor: Colors.transparent,
                 highlightColor: Colors.transparent,
                 onTap: () async {
-                  redirection != null
+                  widget.boolPage
                       ? Navigator.pushReplacement(
                           context,
                           PageRouteBuilder(
                             pageBuilder: (context, animation1, animation2) =>
-                                redirection!,
+                                widget.redirection!,
                             transitionDuration: Duration.zero,
                             reverseTransitionDuration: Duration.zero,
                           ),
                         )
-                      : showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.all(
-                                Radius.circular(20),
-                              ),
-                            ),
-                            // backgroundColor: Colors.deepPurple,
-                            content: const Text(
-                              'Voulez vraiment supprimer votre compte ?',
-                              style: TextStyle(fontSize: 17),
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context, 'Cancel');
-                                },
-                                child: const Text(
-                                  'Non',
-                                  style: TextStyle(
-                                    color: CouleursPrefs.couleurRose,
+                      : widget.boolPopUp
+                          ? showDialog<String>(
+                              context: context,
+                              builder: (BuildContext context) => AlertDialog(
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(20),
                                   ),
                                 ),
-                              ),
-                              TextButton(
-                                onPressed: () async {
-                                  funSupp();
-                                },
-                                child: const Text(
-                                  'Oui',
-                                  style: TextStyle(
-                                    color: CouleursPrefs.couleurRose,
-                                  ),
+                                // backgroundColor: Colors.deepPurple,
+                                content: const Text(
+                                  'Voulez vraiment supprimer votre compte ?',
+                                  style: TextStyle(fontSize: 17),
                                 ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context, 'Cancel');
+                                    },
+                                    child: const Text(
+                                      'Non',
+                                      style: TextStyle(
+                                        color: CouleursPrefs.couleurRose,
+                                      ),
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () async {
+                                      widget.funSupp();
+                                    },
+                                    child: const Text(
+                                      'Oui',
+                                      style: TextStyle(
+                                        color: CouleursPrefs.couleurRose,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        );
+                            )
+                          : null;
                 },
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 25),
@@ -92,18 +110,26 @@ class CaseCompte extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Text(
-                        texte,
+                        widget.texte,
                         style: TextStyle(color: Colors.white, fontSize: 18),
                       ),
                       SizedBox(
                         child: Align(
                           alignment: AlignmentDirectional(0.9, 0),
-                          child: Icon(
-                            Icons.arrow_forward_ios,
-                            color: Colors.white,
-                            // color: Color(0xFF95A1AC),
-                            size: 18,
-                          ),
+                          child: widget.toggle
+                              ? Switch(
+                                  value: widget.activeToggle,
+                                  onChanged: (newValue) {
+                                    setState(() {
+                                      widget.activeToggle = newValue;
+                                    });
+                                  },
+                                )
+                              : Icon(
+                                  Icons.arrow_forward_ios,
+                                  color: Colors.white,
+                                  size: 18,
+                                ),
                         ),
                       ),
                     ],
