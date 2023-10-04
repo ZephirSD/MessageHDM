2f101dab7f138427217b7b76793f055b3f7a756b
ZephirSD <siegfrieddallery@gmail.com>
2023-10-04 21:00:53 +0400

    Ajout de `require` à tous les champs dans le schema `Documents`

--- mongoServer/models/Documents.js
+++ mongoServer/models/Documents.js
@@ -2,16 +2,19 @@ const mongoose = require("mongoose");
 const documentSchema = new mongoose.Schema({
   nom_fichier: {
      type: String,
-     unique: true,
+     require: true,
   },
   extension: {
      type: String,
+     require: true,
   },
   data_document: {
      type: Buffer,
+     require: true,
   },
   idUser: {
     type: String,
+    require: true,
   }
 },{ timestamps: true });
 module.exports = mongoose.model("Documents", documentSchema);
